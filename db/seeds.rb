3669b2a82698885da0987f3c706413f7f051bb99
amendoza514 <amendoza15@cmc.edu>
2020-06-29 16:09:12 -0700

    index page styling complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,26 +21,35 @@ user8 = User.create!(email: "wrrs_blue_a_three1_leed@gmail.com", password:"12345
 user9 = User.create!(email: "orlando_bubble_ball@gmail.com", password:"123456", verified: false, icon_color: '#c4302b')
 
 
-videoInfo1 = Video.create!(user_id: user4.id, views: 2352435, title:"2008 Celtics || Greatest Sports Team in History of the Universe?", description:"Boston sports continue to dominate. Revisit a year that sent shockwaves through the NBA and all of mankind", category:"Sports", tags:["celtics", "nba", "green"], comment_visibility: true)
+videoInfo1 = Video.create!(user_id: user4.id, views: 2352435, title:"2008 Celtics | Greatest Sports Team in History of the Universe?", description:"Boston sports continue to dominate. Revisit a year that sent shockwaves through the NBA and all of mankind", category:"Sports", tags:["celtics", "nba", "green"], comment_visibility: true)
+videoInfo5 = Video.create!(user_id: user1.id, views: 342315, title:"Locomotives in the NBA. Can you call a foul on a train?", description:"NBA refs have a tough decision to make. 1) Call the actual hard foul on a moving vehicle. 2) Give smaller pedestrians right of way, even if it contradicts context. Comment below with your take on fouling LeBron James", category: "Automobiles", tags:["trains", "lebron", "GOAT"], comment_visibility: true)
+videoInfo9 = Video.create!(user_id: user6.id, views: 806235, title:"Spurs Go CRAZY. Layups For DAYS. All Game Highlights", description:"Boring basketball is consistent basketall", category: "Sports", tags:["nba", "team", "sharing"], comment_visibility: true)
 videoInfo2 = Video.create!(user_id: user2.id, views: 7, title:"The Dyansty That Never Was", description:"3 Future MVPs, now just Steven Adams.", category:"History", tags:["nba", "thunder", "dynasty"], comment_visibility: true)
 videoInfo3 = Video.create!(user_id: user9.id, views: 765, title:"BIG Color", description:"Still better than the sleeved jerseys. Comment below on your favorite.", category:"Fashion", tags:["color", "nba", "BIG"], comment_visibility: true)
 videoInfo4 = Video.create!(user_id: user3.id, views: 52312, title:"You Miss 100% of the Hesi Pullup Bankers You Don't Take", description:"5 seconds on the clock, with 2 shooters wide open...Do you take the shot, or do you take the shot?", category:"Lifestyle", tags:["nba", "midrange", "style"], comment_visibility: true)
-videoInfo5 = Video.create!(user_id: user1.id, views: 342315, title:"Locomotives in the NBA. Can you call a foul on a train?", description:"NBA refs have a tough decision to make. 1) Call the actual hard foul on a moving vehicle. 2) Give smaller pedestrians right of way, even if it contradicts context. Comment below with your take on fouling LeBron James", category: "Automobiles", tags:["trains", "lebron", "GOAT"], comment_visibility: true)
 videoInfo6 = Video.create!(user_id: user8.id, views: 765348, title:"Great Ad, Terrible Jerseys: A Story of the League's Worst Ever Uniforms", description:"Comment below with your least favorite unis. #bigPajamas #lebronJustRipsEm #keepingItFresh #yikes", category: "Fashion", tags:["sleeves", "nba", "marketing"], comment_visibility: true)
-videoInfo7 = Video.create!(user_id: user1.id, views: 96352, title:"GOATS || Part 3: Choosing your Footwear", description:"Goats need particular attention when it comes to picking footwear for the job at hand. Here are some helpful tips on picking the right fit", category: "Animals", tags:["GOAT", "nba"], comment_visibility: false)
+videoInfo7 = Video.create!(user_id: user1.id, views: 96352, title:"GOATS | Part 3: Choosing your Footwear", description:"Goats need particular attention when it comes to picking footwear for the job at hand. Here are some helpful tips on picking the right fit", category: "Animals", tags:["GOAT", "nba"], comment_visibility: false)
 videoInfo8 = Video.create!(user_id: user7.id, views: 12, title:"Many Teams to Choose From, Many Reasons to Jump Off the Bandwagon: The Texas Sports Team Problem", description:"Too many teams, not enough attention spans", category: "History", tags:["dallas", "nba", "mavs?"], comment_visibility: true)
-videoInfo9 = Video.create!(user_id: user6.id, views: 806235, title:"Spurs Go CRAZY. Layups For DAYS. All Game Highlights", description:"Boring basketball is consistent basketall", category: "Sports", tags:["nba", "team", "sharing"], comment_visibility: true)
 
 videoFile1 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/2012+Celtics+BIG+Playoffs+Commercial.mp4')
 thumbFile1 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/celtics2.jpg')
 videoInfo1.thumbnail.attach(io: thumbFile1, filename: 'celtics.jpg')
 videoInfo1.video.attach(io: videoFile1, filename: 'celtics.mp4')
 
+videoFile5 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/BIG+LeBron+James.mp4')
+videoInfo5.video.attach(io: videoFile5, filename: 'big_bron.mp4')
+thumbFile5 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/1f4e4ec0ce697db7ebc2bc88179c5443.jpg')
+videoInfo5.thumbnail.attach(io: thumbFile5, filename: 'big_bron.jpg')
+
+videoFile9 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/Spurs+Playoff+Promo+BIG.mp4')
+videoInfo9.video.attach(io: videoFile9, filename: 'spurs.mp4')
+thumbFile9 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/timduncanspurs.jpg')
+videoInfo9.thumbnail.attach(io: thumbFile9, filename: 'spurs.jpg')
+
 videoFile2 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/2012+Oklahoma+City+Thunder+BIG+Playoffs+Promo.mp4')
 videoInfo2.video.attach(io: videoFile2, filename: 'thunder.mp4')
 thumbFile2 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/thunder.jpg')
 videoInfo2.thumbnail.attach(io: thumbFile2, filename: 'thunder.jpg')
-
 
 videoFile3 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/BIG+Color.mp4')
 videoInfo3.video.attach(io: videoFile3, filename: 'color.mp4')
@@ -51,11 +60,6 @@ videoFile4 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/BIG+Kobe+Brya
 videoInfo4.video.attach(io: videoFile4, filename: 'kobe.mp4')
 thumbFile4 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/kobe.jpg')
 videoInfo4.thumbnail.attach(io: thumbFile4, filename: 'kobe.jpg')
-
-videoFile5 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/BIG+LeBron+James.mp4')
-videoInfo5.video.attach(io: videoFile5, filename: 'big_bron.mp4')
-thumbFile5 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/1f4e4ec0ce697db7ebc2bc88179c5443.jpg')
-videoInfo5.thumbnail.attach(io: thumbFile5, filename: 'big_bron.jpg')
 
 videoFile6 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/Jingle+Hoops.mp4')
 videoInfo6.video.attach(io: videoFile6, filename: 'jingle.mp4')
@@ -71,8 +75,3 @@ videoFile8 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/NBA+BIG+-+Go+
 videoInfo8.video.attach(io: videoFile8, filename: 'dallas.mp4')
 thumbFile8 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/mavs_ap.jpg')
 videoInfo8.thumbnail.attach(io: thumbFile8, filename: 'dallas.jpg')
-
-videoFile9 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/Spurs+Playoff+Promo+BIG.mp4')
-videoInfo9.video.attach(io: videoFile9, filename: 'spurs.mp4')
-thumbFile9 = open('https://tutube-seeds.s3-us-west-1.amazonaws.com/timduncanspurs.jpg')
-videoInfo9.thumbnail.attach(io: thumbFile9, filename: 'spurs.jpg')
