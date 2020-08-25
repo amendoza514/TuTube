1575bf326918d6addab4e2789553c340670432d4
amendoza514 <amendoza15@cmc.edu>
2020-08-25 15:23:23 -0700

    created at added to home index view and video previews

--- frontend/components/Video/video.jsx
+++ frontend/components/Video/video.jsx
@@ -2,119 +2,158 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class Video extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = { hover: false }
-        this.videoPreview = this.videoPreview.bind(this)
-        this.videoClose = this.videoClose.bind(this);
-        this.time = Math.floor(Math.random() * 24) + 1;
-        this.span = ["days", "weeks", "hours", "minutes", "months", "years"];
-        this.randomSpan = this.span[Math.floor(Math.random() * this.span.length)];
-        this.descriptionSpan = this.descriptionSpan.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hover: false };
+    this.videoPreview = this.videoPreview.bind(this);
+    this.videoClose = this.videoClose.bind(this);
+    this.datePosted = this.datePosted.bind(this);
+    this.descriptionSpan = this.descriptionSpan.bind(this);
+  }
 
-    usernameParse() {
-        let s = this.props.video.user;
-        let searchString = "@";
-        s = s.slice(0, s.indexOf(searchString))
-        s = s.split('_').join(' ')
-        let words = s.split(' ')
-        let newWords = []
-        words.forEach(word => newWords.push(word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase()));
-        return newWords.join(' ')
-    }
-
-    buttonParse() {
-        let s = this.props.video.user
-        return s.slice(0, 1);
-    }
-
-    viewCounter() {
-        let v = this.props.video.views;
-        let s = v.toString()
-
-        if (s.length === 4) {
-            return s.slice(0, 1) + 'K'
-        } else if (s.length === 5) {
-            return s.slice(0, 2) + 'K'
-        } else if (s.length === 6) {
-            return s.slice(0, 3) + 'K'
-        } else if (s.length > 6) {
-            return s.slice(0, 1) + 'M'
+  datePosted(video_date) {
+    if (video_date) {
+      let monthCalc = parseInt(
+        Date.now() - new Date(video_date).getTime().toString()
+      );
+      let monthsSince = monthCalc / 1000 / 3600 / 24 / 30;
+      if (monthsSince < 1) {
+        let days = Math.floor(monthsSince * 30);
+        if (days < 7) {
+          if (days === 0 || !days) return "today";
+          if (days === 1) return "yesterday";
+          if (days > 1) return `${days} days ago`;
         } else {
-            return s
+          return Math.floor(days / 7) + " weeks ago";
         }
-    }
-
-    videoPreview() {
-        this.setState({ hover: true })
-    }
-
-    videoClose() {
-        this.setState({ hover: false })
-    }
-
-    titleSpan() {
-        let title = this.props.video.title;
-        if (title.split('').length > 65) {
-            return title.split('').slice(0,65).join('') + '...'
+      } else if (Math.floor(monthsSince) < 12) {
+        if (Math.floor(monthsSince) < 1) {
+          return Math.floor(monthsSince) + " months ago";
         } else {
-            return title
+          return "1 month ago";
         }
+      } else {
+        return Math.floor(monthsSince) / 12 + " years ago";
+      }
+    }
+  }
+
+  usernameParse() {
+    let s = this.props.video.user;
+    let searchString = "@";
+    s = s.slice(0, s.indexOf(searchString));
+    s = s.split("_").join(" ");
+    let words = s.split(" ");
+    let newWords = [];
+    words.forEach((word) =>
+      newWords.push(
+        word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()
+      )
+    );
+    return newWords.join(" ");
+  }
+
+  buttonParse() {
+    let s = this.props.video.user;
+    return s.slice(0, 1);
+  }
+
+  viewCounter() {
+    let v = this.props.video.views;
+    let s = v.toString();
+
+    if (s.length === 4) {
+      return s.slice(0, 1) + "K";
+    } else if (s.length === 5) {
+      return s.slice(0, 2) + "K";
+    } else if (s.length === 6) {
+      return s.slice(0, 3) + "K";
+    } else if (s.length > 6) {
+      return s.slice(0, 1) + "M";
+    } else {
+      return s;
+    }
+  }
+
+  videoPreview() {
+    this.setState({ hover: true });
+  }
+
+  videoClose() {
+    this.setState({ hover: false });
+  }
+
+  titleSpan() {
+    let title = this.props.video.title;
+    if (title.split("").length > 65) {
+      return title.split("").slice(0, 65).join("") + "...";
+    } else {
+      return title;
+    }
+  }
+
+  descriptionSpan() {
+    let description = this.props.video.description;
+    if (description.split(" ").length > 40) {
+      return description.split(" ").slice(0, 40).join(" ") + "...";
+    } else {
+      return description;
+    }
+  }
+
+  render() {
+    let vIcon = <i className="fas fa-check-circle"></i>;
+    if (!this.props.video.user_verified) {
+      vIcon = "";
+    }
+    let userIcon = (
+      <>
+        <div
+          className="user-button-video-index"
+          style={{ backgroundColor: this.props.color }}
+        >
+          {this.buttonParse()}
+        </div>
+        <Link to={`/watch/${this.props.video.id}`} className="video-title">
+          {this.titleSpan()}
+        </Link>
+        <div className="video-info">
+          <div className="video-creator">
+            {this.usernameParse()} {vIcon}
+          </div>
+          <div className="video-views">
+            {this.viewCounter()} views • {this.datePosted(this.props.video.created_at)}
+          </div>
+        </div>
+      </>
+    );
+
+    let visual = <img className="thumbnail" src={this.props.video.thumbUrl} />;
+    if (this.state.hover) {
+      visual = (
+        <video
+          className="thumbnail"
+          src={this.props.video.videoUrl}
+          type="video/mp4"
+          autoPlay
+          muted
+        ></video>
+      );
     }
 
-    descriptionSpan() {
-        let description = this.props.video.description;
-        if (description.split(' ').length > 40) {
-            return description.split(' ').slice(0,40).join(' ') + '...';
-        } else {
-            return description;
-        }
-    }
-
-    render() {
-        let vIcon = <i className="fas fa-check-circle"></i>
-        if (!this.props.video.user_verified) {
-            vIcon = ''
-        }
-        let userIcon = (
-                <>
-                <div className='user-button-video-index' style={{ backgroundColor: this.props.color }}>{this.buttonParse()}</div>
-                <Link to={`/watch/${this.props.video.id}`} className='video-title'>
-                    {this.titleSpan()}
-                </Link>
-                <div className='video-info'>
-                    <div className='video-creator'>{this.usernameParse()} {vIcon}</div>
-                <div className='video-views'>{this.viewCounter()} views • {this.time} {this.randomSpan} ago</div>
-                </div>
-                </>
-            )
-
-
-        let visual = <img className='thumbnail' src={this.props.video.thumbUrl} />
-        if (this.state.hover) {
-            visual =
-                <video
-                    className='thumbnail'
-                    src={this.props.video.videoUrl} type="video/mp4"
-                    autoPlay
-                    muted
-                >
-                </video>
-        }
-
-        return (
-            <div>
-                <Link to={`/watch/${this.props.video.id}`} onMouseEnter={this.videoPreview} onMouseLeave={this.videoClose}>
-                    {visual}
-                </Link>
-                <div className='title-container'>
-                    {userIcon}
-                </div>
-                
-            </div>
-        )
-    }
+    return (
+      <div>
+        <Link
+          to={`/watch/${this.props.video.id}`}
+          onMouseEnter={this.videoPreview}
+          onMouseLeave={this.videoClose}
+        >
+          {visual}
+        </Link>
+        <div className="title-container">{userIcon}</div>
+      </div>
+    );
+  }
 }
 
 export default Video;
--- frontend/components/Video/video_preview.jsx
+++ frontend/components/Video/video_preview.jsx
@@ -2,141 +2,143 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class VideoPreview extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {hover: false};
-        this.videoPreview = this.videoPreview.bind(this);
-        this.videoClose = this.videoClose.bind(this);
-        this.time = Math.floor(Math.random() * 24) + 1;
-        this.span = ["days", "weeks", "hours", "minutes", "months", "years"];
-        this.randomSpan = this.span[
-          Math.floor(Math.random() * this.span.length)
-        ];
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hover: false };
+    this.videoPreview = this.videoPreview.bind(this);
+    this.videoClose = this.videoClose.bind(this);
+    this.datePosted = this.datePosted.bind(this);
+  }
 
-    usernameParse() {
-        let s = this.props.video.user;
-        let searchString = "@";
-        s = s.slice(0, s.indexOf(searchString))
-        s = s.split('_').join(' ')
-        let words = s.split(' ')
-        let newWords = []
-        words.forEach(word => newWords.push(word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()));
-        return newWords.join(' ')
-    }
-
-    viewCounter() {
-        let v = this.props.video.views;
-        let s = v.toString()
-
-        if (s.length === 4) {
-            return s.slice(0, 1) + 'K'
-        } else if (s.length === 5) {
-            return s.slice(0, 2) + 'K'
-        } else if (s.length === 6) {
-            return s.slice(0, 3) + 'K'
-        } else if (s.length > 6) {
-            return s.slice(0, 1) + 'M'
+  datePosted(video_date) {
+    if (video_date) {
+      let monthCalc = parseInt(
+        Date.now() - new Date(video_date).getTime().toString()
+      );
+      let monthsSince = monthCalc / 1000 / 3600 / 24 / 30;
+      if (monthsSince < 1) {
+        let days = Math.floor(monthsSince * 30);
+        if (days < 7) {
+          if (days === 0 || !days) return "today";
+          if (days === 1) return "yesterday";
+          if (days > 1) return `${days} days ago`;
         } else {
-            return s
+          return Math.floor(days / 7) + " weeks ago";
         }
-    }
-    
-    viewDisplay() {
-        let v = this.props.video.views;
-        let s = v.toString()
-        return s.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-    }
-
-    titleSpan() {
-        let title = this.props.video.title;
-        if (title.split('').length > 50) {
-            return title.split('').slice(0, 50).join('') + '...'
+      } else if (Math.floor(monthsSince) < 12) {
+        if (Math.floor(monthsSince) < 1) {
+          return Math.floor(monthsSince) + " months ago";
         } else {
-            return title
+          return "1 month ago";
         }
+      } else {
+        return Math.floor(monthsSince) / 12 + " years ago";
+      }
+    }
+  }
+
+  usernameParse() {
+    let s = this.props.video.user;
+    let searchString = "@";
+    s = s.slice(0, s.indexOf(searchString));
+    s = s.split("_").join(" ");
+    let words = s.split(" ");
+    let newWords = [];
+    words.forEach((word) =>
+      newWords.push(
+        word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()
+      )
+    );
+    return newWords.join(" ");
+  }
+
+  viewCounter() {
+    let v = this.props.video.views;
+    let s = v.toString();
+
+    if (s.length === 4) {
+      return s.slice(0, 1) + "K";
+    } else if (s.length === 5) {
+      return s.slice(0, 2) + "K";
+    } else if (s.length === 6) {
+      return s.slice(0, 3) + "K";
+    } else if (s.length > 6) {
+      return s.slice(0, 1) + "M";
+    } else {
+      return s;
+    }
+  }
+
+  viewDisplay() {
+    let v = this.props.video.views;
+    let s = v.toString();
+    return s.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+  }
+
+  titleSpan() {
+    let title = this.props.video.title;
+    if (title.split("").length > 50) {
+      return title.split("").slice(0, 50).join("") + "...";
+    } else {
+      return title;
+    }
+  }
+
+  videoPreview() {
+    this.setState({ hover: true });
+    // setTimeout(() => this.setState({ hover: true }), 500)
+  }
+
+  videoClose() {
+    this.setState({ hover: false });
+  }
+
+  render() {
+    let vIcon = <i className="fas fa-check-circle"></i>;
+    if (!this.props.video.user_verified) {
+      vIcon = "";
     }
 
-    videoPreview() {
-        this.setState({ hover: true })
-        // setTimeout(() => this.setState({ hover: true }), 500)
-    }
-
-    videoClose() {
-        this.setState({hover: false})
-    }
-
-    render() {
-
-        let vIcon = <i className="fas fa-check-circle" ></i>
-        if (!this.props.video.user_verified) {
-            vIcon = '';
-        }
-
-
-        let visual = <img className='small-thumb' src={this.props.video.thumbUrl} />
-        if (this.state.hover) {
-            visual =
+    let visual = (
+      <img className="small-thumb" src={this.props.video.thumbUrl} />
+    );
+    if (this.state.hover) {
+      visual = (
         <video
-            className='small-thumb'
-            src={this.props.video.videoUrl} type="video/mp4"
-            autoPlay
-            muted
-        >
-        </video>
-        }
-        return (
-            <>
+          className="small-thumb"
+          src={this.props.video.videoUrl}
+          type="video/mp4"
+          autoPlay
+          muted
+        ></video>
+      );
+    }
+    return (
+      <>
+        <div>
+          <div onMouseEnter={this.videoPreview} onMouseLeave={this.videoClose}>
+            <Link to={`/watch/${this.props.video.id}`}>
+              {visual}
 
+              <div className="preview-title">{this.titleSpan()}</div>
+              <div className="preview-title">{this.titleSpan()}</div>
 
-                < div >
-                    <div onMouseEnter={this.videoPreview} onMouseLeave={this.videoClose}> 
-                        <Link to={`/watch/${this.props.video.id}`}  >
-                            {visual}
-
-                            <div className='preview-title'>{this.titleSpan()}</div>
-                            <div className='preview-title'>{this.titleSpan()}</div>
-                       
-                        {/* <Link to={`/watch/${this.props.video.id}`} className='preview-title'>
+              {/* <Link to={`/watch/${this.props.video.id}`} className='preview-title'>
                             {this.titleSpan()}
                         </Link> */}
-                    
-                    <div className='preview-creator'>{this.usernameParse()} {vIcon}</div>
-                    <div className='preview-views'>{this.viewCounter()} views • {this.time} {this.randomSpan} ago</div>
-                     </Link>
-                    </div>
-                </div >
-                
-            </>
-        )
-    }
+
+              <div className="preview-creator">
+                {this.usernameParse()} {vIcon}
+              </div>
+              <div className="preview-views">
+                    {this.viewCounter()} views • {this.datePosted(this.props.video.created_at)}
+              </div>
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-//FIX seperate link within link
+
 export default VideoPreview
-
-
-    // < div >
-    // <div>
-    //     <Link to={`/watch/${this.props.video.id}`}  >
-    //         <img className='small-thumb' src={this.props.video.thumbUrl} />
-    //     </Link>
-    //     <Link to={`/watch/${this.props.video.id}`} className='preview-title'>
-    //         {this.titleSpan()}
-    //     </Link>
-    // </div>
-    // <div className='preview-creator'>{this.usernameParse()} {vIcon}</div>
-    // <div className='preview-views'>{this.viewCounter()} views • {randomNumber} {randomSpan} ago</div>
-    //             </div >
-
-
-    // < div >
-    // <div></div>
-    // <Link to={`/watch/${this.props.video.id}`}  >
-    //     <img className='small-thumb' src={this.props.video.thumbUrl} />
-    //     <Link to={`/watch/${this.props.video.id}`} className='preview-title'>
-    //         {this.titleSpan()}
-    //     </Link>
-    // </Link>
-    // <div className='preview-creator'>{this.usernameParse()} {vIcon}</div>
-    // <div className='preview-views'>{this.viewCounter()} views • {randomNumber} {randomSpan} ago</div>
-    //             </div >
