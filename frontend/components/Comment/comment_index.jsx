6041764caba32403794c817e8fcb1f741e3317f5
amendoza514 <amendoza15@cmc.edu>
2020-10-07 14:20:30 -0700

    initial sorting via front end only

--- frontend/components/Comment/comment_index.jsx
+++ frontend/components/Comment/comment_index.jsx
@@ -3,57 +3,79 @@ import CommentIndexItem from './comment_index_item'
 import CommentForm from './comment_form'
 
 class CommentIndex extends React.Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
+    this.state = { sorted: false };
+    this.handleSort = this.handleSort.bind(this);
+  }
+
+  buttonParse() {
+    let s = this.props.currentUser.email;
+    return s.slice(0, 1);
+  }
+
+  handleSort(e) {
+    e.preventDefault();
+      if (!this.state.sorted) this.setState({ sorted: true });
+      if (this.state.sorted) this.setState({ sorted: false });
+  }
+
+  render() {
+    let revArr = [];
+
+    if (!this.state.sorted) {
+      for (let i = this.props.comments.length - 1; i >= 0; i--) {
+        revArr.push(this.props.comments[i]);
+      }
+    } else {
+      let sorted = this.props.comments
+        .sort((a, b) => a.likes.length - b.likes.length)
+        .slice();
+      revArr = sorted;
     }
 
+    let countStyle =
+      this.props.mode === false ? "comments-count" : "dark-comments-count";
+    let sortStyle = this.props.mode === false ? "sort-icon" : "dark-sort-icon";
 
-    buttonParse() {
-        let s = this.props.currentUser.email
-        return s.slice(0, 1);
-    }
-
-    render() {
-        let revArr = [];
-        for (let i = this.props.comments.length - 1; i >=0; i--) {
-            revArr.push(this.props.comments[i])
-        }
-
-        let countStyle = this.props.mode === false ? "comments-count" : "dark-comments-count";
-        let sortStyle = this.props.mode === false ? "sort-icon" : "dark-sort-icon";
-
-        return (
-            <>
-                <div className={countStyle}>
-                    {this.props.comments.length} 
-                    <div className="comments-text">Comments</div>
-                    <div className={sortStyle}><i className="fas fa-sort"></i>&nbsp;&nbsp;SORT BY</div>
-                </div>
-                <CommentForm 
-                    currentUser={this.props.currentUser} 
-                    currentVideo={this.props.match.params.videoId} 
-                    createComment={this.props.createComment} 
-                    mode={this.props.mode}
-                    />
-                <div className='comment-container'>
-                    {revArr.map((comment, idx) => { return(
-                    <CommentIndexItem 
-                      key={idx} currentUser={this.props.currentUser} 
-                      comment={comment} 
-                      deleteComment={this.props.deleteComment} 
-                      updateComment = {this.props.updateComment}
-                      likeComment={this.props.likeComment}
-                      dislikeComment={this.props.dislikeComment}
-                      destroyCommentLike={this.props.destroyCommentLike}
-                      user={comment.user} 
-                      userColor ={comment.userColor} 
-                      mode={this.props.mode}
-                      />)
-                    })}
-                </div>
-            </>
-        )
-    }
+    return (
+      <>
+        <div className={countStyle}>
+          {this.props.comments.length}
+          <div className="comments-text">Comments</div>
+          <div className={sortStyle} onClick={this.handleSort}>
+            <i className="fas fa-sort"></i>
+            &nbsp;&nbsp;SORT BY
+          </div>
+        </div>
+        <CommentForm
+          currentUser={this.props.currentUser}
+          currentVideo={this.props.match.params.videoId}
+          createComment={this.props.createComment}
+          mode={this.props.mode}
+        />
+        <div className="comment-container">
+          {revArr.map((comment, idx) => {
+            return (
+              <CommentIndexItem
+                key={idx}
+                currentUser={this.props.currentUser}
+                comment={comment}
+                deleteComment={this.props.deleteComment}
+                updateComment={this.props.updateComment}
+                likeComment={this.props.likeComment}
+                dislikeComment={this.props.dislikeComment}
+                destroyCommentLike={this.props.destroyCommentLike}
+                user={comment.user}
+                userColor={comment.userColor}
+                mode={this.props.mode}
+              />
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
 
 export default CommentIndex;
